a4785202f34e50f03d2224822761ddf7b4b03cc1
EzeCuevas <eze.cuevass@gmail.com>
2024-10-13 15:32:34 +0000

    ADD: punto F agregado

---
+++ puntof.sh
@@ -0,0 +1,4 @@
+!#/bin/bash
+echo "Mi ip publica es: $(curl -s ifconfig.me)" >> filtroavanzado.txt
+echo "Mi usuario es: $(whoami)" >> filtroavanzado.txt
+echo "El hash de mi usuario es: $(sudo cat /etc/shadow | grep vagrant | awk -F ':' '{print $2}')" >> filtroavanzado.txt
