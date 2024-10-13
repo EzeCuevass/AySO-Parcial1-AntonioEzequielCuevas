2929d53a9a8418ca010e05ef0b1fa88f0f1afd91
Ezequiel Cuevas <87551668+EzeCuevass@users.noreply.github.com>
2024-10-13 16:02:48 -0300

    ADD: Punto B

--- puntob.sh
+++ puntob.sh
@@ -5,4 +5,8 @@ sudo mkfs.ext4 /dev/sdc1
 sudo mkfs.ext4 /dev/sdc2
 sudo mkfs.ext4 /dev/sdc3
 sudo mkfs.ext4 /dev/sdc4
+sudo mount /dev/sdc1 /mnt/sdc1
+sudo mount /dev/sdc1 /mnt/sdc2
+sudo mount /dev/sdc1 /mnt/sdc3
+sudo mount /dev/sdc1 /mnt/sdc4
 lsblk
